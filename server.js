075e9b5b2a6fc9478537dc48fbc154e76554a058
Eric Steinberg <ericms@utexas.edu>
2018-01-28 19:06:50 -0600

    Added https, formatting

--- server.js
+++ server.js
@@ -1,25 +1,41 @@
 // Dependencies
-const express = require("express");
-const app = require("express")();
-const Routes = require("./server/config/routes.js");
-const BodyParser = require("body-parser");
-const Promise = require("bluebird");
-const Path = require("path");
-const http = require("http").Server(app);
+const express = require('express');
+const Promise = require('bluebird');
+const app = require('express')();
+const https = require('https');
+const BodyParser = require('body-parser');
+const Path = require('path');
+const fs = require('fs');
+const Routes = require('./server/config/routes.js');
+
+// SSL Certs
+const options = process.env.NODE_ENV === 'production'
+  ? {
+      cert: fs.readFileSync(
+        Path.join(__dirname, '../', 'encryption/fullchain.pem')
+      ),
+      key: fs.readFileSync(
+        Path.join(__dirname, '../', '/encryption/privkey.pem')
+      ),
+      ca: fs.readFileSync(Path.join(__dirname, '../', 'encryption/chain.pem'))
+    }
+  : {};
 
 // Add Body Parser
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: false }));
 
 // Add static content directory (img, css, js, etc)
-app.use(express.static(Path.join(__dirname, "client", "public")));
+app.use(express.static(Path.join(__dirname, 'client', 'public')));
 
 // Routes
-app.use("/", Routes);
+app.use('/', Routes);
 
 // Server listen
-http.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on ${process.env.PORT || 3000}`);
 });
+
+https.createServer(options, app).listen(8443);
 
 module.exports = app;
